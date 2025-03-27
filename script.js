1f45b723bddd13222012bb957c88a06351e1019e
ArthurASB <195683488+ArthurASB@users.noreply.github.com>
2025-03-27 00:44:17 -0300

    Add colors to mouse trail

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function randomColor () {
+    let rgbColor = "";
+    const rgbFirst = Math.floor(Math.random()*255);
+    const rgbSecond = Math.floor(Math.random()*255);
+    const rgbThird = Math.floor(Math.random()*255);
+    rgbColor = `rgb(${rgbFirst}, ${rgbSecond}, ${rgbThird})`;
+    return rgbColor;
+}
+
 const container = document.querySelector(".container");
 
 let num = 100;
@@ -18,6 +27,11 @@ const hoverBoxes = document.querySelectorAll(".columnbox");
 
 hoverBoxes.forEach((box) => {
     box.addEventListener("mouseenter", () => {
-        box.style["background-color"] = "black";
+        box.style["background-color"] = randomColor();
+        // box.style["oppacity"] = box.style["oppacity"] - 0.1;
     });
 });
+
+
+
+//Math.floor(Math.random()*255);
