f61eb4b2533cb9408a02ba61ba0dbda137949419
ArthurASB <195683488+ArthurASB@users.noreply.github.com>
2025-03-27 20:57:06 -0300

    Add user prompt and grid recreation

--- script.js
+++ script.js
@@ -7,36 +7,47 @@ function randomColor () {
     return rgbColor;
 }
 
-function createGrid (num=50){
-    const container = document.querySelector(".container");
+function createGrid (container, num=50){
+    for (let i=0; i < num; i++){
+        let rowBox = document.createElement("div");
+        rowBox.classList.add("rowbox");
+        container.appendChild(rowBox);
 
-// let num = 50;
+        for (let j=0; j < num; j++){
+            let columnBox = document.createElement("div");
+            columnBox.classList.add("columnbox");
+            rowBox.appendChild(columnBox);
+        };
+    };
 
-for (let i=0; i < num; i++){
-    let rowBox = document.createElement("div");
-    rowBox.classList.add("rowbox");
-    container.appendChild(rowBox);
+    const hoverBoxes = document.querySelectorAll(".columnbox");
 
-    for (let j=0; j < num; j++){
-        let columnBox = document.createElement("div");
-        columnBox.classList.add("columnbox");
-        rowBox.appendChild(columnBox);
-    }
-}
-}
-
-createGrid();
-
-const hoverBoxes = document.querySelectorAll(".columnbox");
-
-hoverBoxes.forEach((box) => {
-    box.addEventListener("mouseenter", () => {
-        box.style["background-color"] = randomColor();
-        let curOpacity = parseFloat(box.style.opacity) || 0;
-        box.style.opacity = Math.min(1, curOpacity + 0.1);
+    hoverBoxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.style["background-color"] = randomColor();
+            let curOpacity = parseFloat(box.style.opacity) || 0;
+            box.style.opacity = Math.min(1, curOpacity + 0.1);
+        });
     });
+}
+
+const contDiv = document.querySelector(".container");
+createGrid(contDiv);
+
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=>{
+    let newNum = prompt("Please input the number that will equally determine the lines and rows" +
+        "\n(only one number and it should be smaller than 100)"
+    );
+    if (newNum > 100){
+        alert ("You've entered an invalid number, please try again");
+    } else {
+                // contDiv.innerHTML= "";
+        while (contDiv.firstChild){
+            contDiv.removeChild(contDiv.firstChild);
+        }
+        createGrid(contDiv, newNum);
+    }
 });
-
-
-
-//Math.floor(Math.random()*255);
