5b78d2fbd14a8f4ec1c0b310a50cd03d936576e8
ArthurASB <195683488+ArthurASB@users.noreply.github.com>
2025-03-26 21:06:07 -0300

    Add two-dimensional logicand grid dimensions

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const container = document.querySelector(".container");
 
-for (let i =0; i < 10; i++){
-    let gridBox = document.createElement("div");
-    gridBox.classList.add("gridbox")
-    container.appendChild(gridBox);
+let num = 10;
+
+for (let i=0; i < num; i++){
+    let rowBox = document.createElement("div");
+    rowBox.classList.add("rowbox");
+    container.appendChild(rowBox);
+
+    for (let j=0; j < num; j++){
+        let columnBox = document.createElement("div");
+        columnBox.classList.add("columnbox");
+        rowBox.appendChild(columnBox);
+    }
 }
